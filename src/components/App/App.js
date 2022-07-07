2ea797da7e329fbdfcd34bfd7b6b15ca133291c3
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-07-07 15:28:49 -0700

    cleaning up this app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,19 +4,7 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
 import yelp from '../../utilis/Yelp'
-// const business = {
-//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-//   name: 'MarginOtto Pizzeria',
-//   address: '1010 Paddington Way',
-//   city: 'Flavortown',
-//   state: 'NY',
-//   zipCode: '10101',
-//   category: 'Italian',
-//   rating: 4.5,
-//   reviewCount: 90
-// };
 
-// const businesses = [business, business, business, business, business, business,]
 export default class App extends React.Component {
   constructor(props) {
     super(props)
@@ -33,7 +21,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList business={this.state.businesses} />
       </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -66,7 +66,7 @@ export default class SearchBar extends React.Component {
                     <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
-                    <a href='' onClick={this.handleSearch}>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         );
