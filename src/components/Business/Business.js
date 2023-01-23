7b6443398756f708acf8497d8acc19433f996cd4
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-01-22 16:15:00 -0800

    updating project

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -8,21 +8,23 @@ export default class Business extends React.Component {
 
     render() {
         return (
-            <div className="Business">
+            <div className="Business card">
                 <div className="image-container">
                     <img src={this.props.business.imageSrc} alt='' />
                 </div>
-                <h2>{this.props.business.name}</h2>
-                <div className="Business-information">
-                    <div className="Business-address">
-                        <p>{this.props.business.address}</p>
-                        <p>{this.props.business.city}</p>
-                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
-                    </div>
-                    <div className="Business-reviews">
-                        <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating}</h3>
-                        <p>{this.props.business.reviewCount} reviews</p>
+                <h2 className='business-name'>{this.props.business.name}</h2>
+                <div className='card-content'>
+                    <div className="Business-information content">
+                        <div className="Business-address">
+                            <p>{this.props.business.address}</p>
+                            <p>{this.props.business.city}</p>
+                            <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        </div>
+                        <div className="Business-reviews">
+                            <h5>{this.props.business.category}</h5>
+                            <h3 className="rating">{this.props.business.rating}</h3>
+                            <p>{this.props.business.reviewCount} reviews</p>
+                        </div>
                     </div>
                 </div>
             </div>
